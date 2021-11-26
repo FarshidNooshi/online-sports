1a22f5787d1b27a995dcd6e097177174c7c97d2f
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2021-11-26 20:37:53 +0330

    update auth.php

--- online-sports/routes/auth.php
+++ online-sports/routes/auth.php
@@ -10,55 +10,71 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisteredUserController::class, 'create'])
+//setting the required functionalities for auth.php
+Route::get('/register', [RegisteredUserController::class, 'create'])// handled for registering 
                 ->middleware('guest')
                 ->name('register');
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
+
+Route::post('/register', [RegisteredUserController::class, 'store']) // for storing 
                 ->middleware('guest');
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create']) // for creating in login
                 ->middleware('guest')
                 ->name('login');
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])// for storing 
                 ->middleware('guest');
 
-Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
+
+Route::get('/forgot-password', [PasswordResetLinkController::class, 'create']) // forgetting password request
                 ->middleware('guest')
                 ->name('password.request');
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])// email, forgetting password
                 ->middleware('guest')
                 ->name('password.email');
 
-Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+
+Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])// resetting password and token get
                 ->middleware('guest')
                 ->name('password.reset');
 
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])// resetting password and update post
                 ->middleware('guest')
                 ->name('password.update');
 
-Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+
+Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])// validation for email
                 ->middleware('auth')
                 ->name('verification.notice');
 
-Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+
+Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])// verification for email, id
                 ->middleware(['auth', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])// email post
                 ->middleware(['auth', 'throttle:6,1'])
                 ->name('verification.send');
 
-Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+
+Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])// confirmation for password
                 ->middleware('auth')
                 ->name('password.confirm');
 
-Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+
+Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])//posting confirmation of password
                 ->middleware('auth');
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])//logging out, post
                 ->middleware('auth')
                 ->name('logout');
+
+                
