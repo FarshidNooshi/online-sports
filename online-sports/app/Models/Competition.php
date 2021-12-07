4cacd8725fbb2a325d170e43670d895ca981f3c4
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-07 11:50:26 +0330

    LSR-4 competition model initialized

---
+++ online-sports/app/Models/Competition.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Competition is our football leagues.
+ * For separating teams of the countries into
+ * parts.
+ *
+ */
+class Competition extends Model
+{
+    // Traits
+    use HasFactory;
+
+    /**
+     * Competition fields.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+    ];
+}
