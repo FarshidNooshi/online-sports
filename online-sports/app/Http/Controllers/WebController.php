65ebb316784d4ba21d70ce6e574115ce54a11fbe
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-08 18:22:12 +0330

    LSR-4 Web controller added

---
+++ online-sports/app/Http/Controllers/WebController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Competition;
+use App\Models\Country;
+use App\Models\Team;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+ * Web controller handles the requests API.
+ *
+ */
+class WebController extends Controller
+{
+    /**
+     * The index method handles the home route data.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $countries = Country::all();
+        $competitions = Competition::all();
+        $teams = Team::all();
+
+        return response()
+            ->json([
+                'countries' => $countries,
+                'competitions' => $competitions,
+                'teams' => $teams
+            ]);
+    }
+}
