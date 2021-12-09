f7beb7aef189b6a28221b427f3c6cd065b083b6d
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2021-12-10 02:32:44 +0330

    LSR-21 small changes in font size and color

--- online-sports/resources/views/components/match/match-result.blade.php
+++ online-sports/resources/views/components/match/match-result.blade.php
@@ -1,15 +1,15 @@
-<div class="grid gap-4 place-items-center bg-gray-800 h-16 w-full border-b border-gray-900" style="grid-template-columns: 1fr 25px 40px 25px 1fr;">
-    <span class="text-right w-full">{{ $match['match_hometeam_name'] }}</span>
+<div class="grid gap-4 place-items-center bg-gray-800 h-16 w-full border-b border-gray-900 hover:bg-gray-700 transition" style="grid-template-columns: 1fr 25px 40px 25px 1fr;">
+    <span class="text-right w-full text-sm">{{ $match['match_hometeam_name'] }}</span>
     <img class="" width="25" height="25" src="{{ $match['team_home_badge'] }}">
     <div class="grid grid-flow-row gap-1 justify-items-center grid-rows-1">
         @if($match['match_live'] == 1 or $match['match_status'] == 'Finished')
             <span class="">{{ $match['match_hometeam_score'] }} - {{ $match['match_awayteam_score'] }}</span>
-            <span class="text-gray-500 text-sm">{{ $match['match_status'] }}</span>
+            <span class="text-xs text-green-600">{{ $match['match_status'] }}</span>
         @else
             <span class="text-gray-500">{{ $match['match_time'] }}</span>
         @endif
 
     </div>
     <img class="" width="25" height="25" src="{{ $match['team_away_badge'] }}">
-    <span class="text-left w-full">{{ $match['match_awayteam_name'] }}</span>
+    <span class="text-left w-full text-sm">{{ $match['match_awayteam_name'] }}</span>
 </div>
--- online-sports/resources/views/components/match/results-table.blade.php
+++ online-sports/resources/views/components/match/results-table.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-gray-700">
         <div class="flex pl-5 h-11 items-center border-b border-gray-900">
             <img height="20" width="20" src="{{ $league['league_badge']  }}" alt="">
-            <span class="ml-4">
+            <span class="ml-4 text-sm font-mono">
                 {{ $league['league_name'] }}
             </span>
         </div>
