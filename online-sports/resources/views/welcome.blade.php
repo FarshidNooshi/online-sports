7e2ded3fb2e7151e245891a6de9e3394c36af995
AmirSpurs <hashemiamirreza@hotmail.com>
2021-12-10 17:07:30 +0330

    LSR-25 added datepicker input

--- online-sports/resources/views/welcome.blade.php
+++ online-sports/resources/views/welcome.blade.php
@@ -37,9 +37,16 @@
                 </div>
             @endif
 
-            <div class="container mt-14 mx-auto p-3 max-w-xl text-white" id="matches_container">
-                <div class="animate-pulse flex flex-col">
-                    @for ($i = 0; $i < 10; $i++)
+            <div class="container mt-14 mx-auto p-3 max-w-xl">
+                <div class="relative mb-1">
+                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <input datepicker type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+                </div>
+                <div id="matches_container" class="text-white">
+                    <div class="animate-pulse flex flex-col">
+                        @for ($i = 0; $i < 10; $i++)
                         <div class="grid gap-4 place-items-center bg-gray-800 h-16 w-full border-b border-gray-900" style="grid-template-columns: 1fr 25px 40px 25px 1fr;">
                             <div class="bg-gray-300 w-20 h-3 ml-auto"></div>
                             <div class="bg-gray-300 rounded-full h-6 w-6"></div>
@@ -49,10 +56,13 @@
                             <div class="bg-gray-300 rounded-full h-6 w-6"></div>
                             <div class="bg-gray-300 w-20 h-3 mr-auto"></div>
                         </div>    
-                    @endfor
+                        @endfor
+                    </div>
                 </div>
             </div>
         </div>
+
+        <script src="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/datepicker.bundle.js"></script>
         <script>
 
             const getMatches = () => {
