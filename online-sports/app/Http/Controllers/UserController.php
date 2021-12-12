391308f2bb5b32dc014f73fa4dc5bef17a30b516
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-12 17:41:56 +0330

    LSR-28 user index for favorite teams method refactored

--- online-sports/app/Http/Controllers/UserController.php
+++ online-sports/app/Http/Controllers/UserController.php
@@ -13,53 +13,18 @@ use Illuminate\Support\Facades\DB;
  */
 class UserController extends Controller
 {
-    // Website API key
-    protected $API_KEY = "e017ebb8a6a65ad9c26acf1eb955aaa8a334da8e1530cb9a7e522f1738818a21";
-
     /**
      * Display a listing of the resource.
      *
-     * @param Request $request
      * @return JsonResponse
      */
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
-        $from = $request->get('from', date('Y-m-d'));
-        $to = $request->get('to', date('Y-m-d'));
-
-        $teams = Auth::user()->teams(Auth::id());
-        $data = [];
-
-        foreach ($teams as $team) {
-            $league_id = $team->league_id;
-            $APIkey = $this->API_KEY;
-            $URL = "https://apiv3.apifootball.com/?action=get_events&from=$from&to=$to&league_id=$league_id&APIkey=$APIkey";
-
-            $curl = curl_init($URL);
-            curl_setopt($curl, CURLOPT_URL, $URL);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-            $resp = curl_exec($curl);
-            curl_close($curl);
-
-            $response = json_decode($resp, true);
-            $obj = [];
-            $obj['team'] = $team;
-
-            foreach ($response as $match) {
-                if ($match['match_hometeam_id'] == $team->team_key || $match['match_awayteam_id'] == $team->team_key) {
-                    $obj['match'][] = $match;
-                }
-            }
-
-            $data[] = $obj;
-        }
+        $teams = Auth::user()
+            ->teams(Auth::id());
 
         return response()
-            ->json($data);
+            ->json($teams);
     }
 
     /**
